865e111ef9e3c0efa0ca653632e6d65110938d07
Fehlpe <felpearcanjo@gmail.com>
2022-12-08 18:19:51 -0300

    ex 2 progAsync

--- progAsync/src/index.ts
+++ progAsync/src/index.ts
@@ -27,3 +27,40 @@ async function getRepositories(repo: string) {
 
 
 // --------------------------------------------------------------------- EXERCICIO 2 ------------------------------
+
+import express, {Express, query, Request, Response} from 'express';
+import { json } from "stream/consumers";
+const server: Express = express();
+
+server.use(express.json())
+
+server.get('/calculadora', (req: Request, res: Response) => {
+    const operacao = req.query.operacao
+    const valorA = req.query.valorA
+    const valorB = req.query.valorB
+    const numA = Number(valorA) 
+    const numB = Number(valorB) 
+    let resultado
+    switch(operacao) {
+        case "somar":
+            resultado = numA + numB
+            break
+        case "subtrair":
+            resultado = numA - numB
+            break
+        case "multiplicar":
+            resultado = numA * numB
+            break
+        case "dividir":
+            resultado = numA / numB
+            break
+        default:
+            res.send("seloco")
+            break
+    }
+    res.send(resultado?.toString())
+})
+
+server.listen(8081, () => {
+    console.log("Servidor OK");
+  });

87b7ca143aee769829cdaeadf5ed5cfdb9504f92
Fehlpe <felpearcanjo@gmail.com>
2022-12-08 18:33:22 -0300

    ex 3 progAsync

--- progAsync/src/index.ts
+++ progAsync/src/index.ts
@@ -28,39 +28,64 @@ async function getRepositories(repo: string) {
 
 // --------------------------------------------------------------------- EXERCICIO 2 ------------------------------
 
-import express, {Express, query, Request, Response} from 'express';
-import { json } from "stream/consumers";
-const server: Express = express();
+// DESCOMENTAR O CODIGO TODO PARA FUNCIONAR
 
-server.use(express.json())
+// import express, {Express, query, Request, Response} from 'express';
+// const server: Express = express();
 
-server.get('/calculadora', (req: Request, res: Response) => {
-    const operacao = req.query.operacao
-    const valorA = req.query.valorA
-    const valorB = req.query.valorB
-    const numA = Number(valorA) 
-    const numB = Number(valorB) 
-    let resultado
-    switch(operacao) {
-        case "somar":
-            resultado = numA + numB
-            break
-        case "subtrair":
-            resultado = numA - numB
-            break
-        case "multiplicar":
-            resultado = numA * numB
-            break
-        case "dividir":
-            resultado = numA / numB
-            break
-        default:
-            res.send("seloco")
-            break
-    }
-    res.send(resultado?.toString())
-})
+// server.use(express.json())
 
-server.listen(8081, () => {
-    console.log("Servidor OK");
-  });
+// server.get('/calculadora', (req: Request, res: Response) => {
+//     const operacao = req.query.operacao
+//     const valorA = req.query.valorA
+//     const valorB = req.query.valorB
+//     const numA = Number(valorA) 
+//     const numB = Number(valorB) 
+//     let resultado
+//     switch(operacao) {
+//         case "somar":
+//             resultado = numA + numB
+//             break
+//         case "subtrair":
+//             resultado = numA - numB
+//             break
+//         case "multiplicar":
+//             resultado = numA * numB
+//             break
+//         case "dividir":
+//             resultado = numA / numB
+//             break
+//         default:
+//             res.send("seloco")
+//             break
+//     }
+//     res.send(resultado?.toString())
+// })
+
+// server.listen(8081, () => {
+//     console.log("Servidor OK");
+//   });
+
+// --------------------------------------------------------------------- EXERCICIO 3 ------------------------------
+
+// DESCOMENTAR O CODIGO TODO PARA FUNCIONAR
+
+// import express, {Express, query, Request, Response} from 'express';
+// const server: Express = express();
+
+// let contador = 0;
+
+// server.get('/contador', (req: Request, res: Response) => {
+//   contador++;
+
+//   res.send(contador.toString())
+
+//   if (contador === 10) {
+//     console.log('Chegou à 10');
+//     contador = 0;
+//   }
+// });
+
+// server.listen(8083, () => {
+//   console.log(`Server OK`);
+// });

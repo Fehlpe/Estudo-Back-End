7f3aaab88054219a7af28f88b2716e49d15eb8b3
Fehlpe <felpearcanjo@gmail.com>
2022-12-12 22:38:08 -0300

    post users, get e get com filtro(incompleto)

--- apiTransactions/src/index.ts
+++ apiTransactions/src/index.ts
@@ -1,1 +1,80 @@
-console.log("toma")
+import express from "express";
+import cors from "cors";
+import { User, Transaction } from "./models/index";
+
+const app = express();
+const users: User[] = [];
+
+app.use(express.json(), cors());
+
+app.post("/users", (req, res) => {
+  const { name, cpf, email, age } = req.body;
+
+  if (!name || !cpf || !email || !age) {
+    res.status(400).json({ error: "Dados obrigatórios não registrados" });
+  } else {
+    const cpfExists = users.some((user) => user.cpf === cpf);
+    if (cpfExists) {
+      return res.status(400).json({ error: "CPF já está em uso" });
+    }
+    const user = new User(name, cpf, email, age);
+    users.push(user);
+    return res.json(user);
+  }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  if (!id) {
+    res.status(418).json({ error: "Necessário informar ID" });
+  } else {
+    const user = users.find((user) => user.id === id);
+
+    if (!user) {
+      return res.status(404).send("Usuário não encontrado.");
+    }
+
+    res.send(user);
+  }
+});
+
+app.get("/users", (req, res) => {
+  const { name, email, cpf } = req.query;
+
+  if (name) {
+    const user = users.find((user) => user.name === name);
+    
+    if(user){
+        return res.json({
+            sucesso: true,
+            data:{
+                      id: user.id,
+                      name: user.name,
+                      cpf: user.cpf,
+                      email: user.email,
+                      age: user.age,
+                  }
+                
+        });
+    } else {
+        res.status(400).json({ error: "Nome não registrado no sistema"})
+    }
+  } else if (email) {
+    return res.json({
+      sucesso: true,
+      data: users.filter((user) => user.email === email),
+    });
+  } else if (Number(cpf)) {
+    return res.json({
+      sucesso: true,
+      data: users.filter((user) => user.cpf === cpf),
+    });
+  } else {
+    return res.json({
+      sucesso: true,
+      data: users,
+    });
+  }
+});
+
+app.listen(8081, () => console.log("Server OK"));
--- apiTransactions/src/models/models.ts
+++ apiTransactions/src/models/models.ts
@@ -1,4 +1,6 @@
-class User {
+import { v4 } from 'uuid'
+
+export class User {
     id: number | string;
     name: string;
     cpf: string;
@@ -7,7 +9,7 @@ class User {
     transactions: Transaction[];
   
     constructor(name: string, cpf: string, email: string, age: number) {
-      this.id = Math.random(); // gera um ID numérico aleatório
+      this.id = v4();
       this.name = name;
       this.cpf = cpf;
       this.email = email;
@@ -16,14 +18,14 @@ class User {
     }
   }
   
-  class Transaction {
+export class Transaction {
     id: number | string;
     title: string;
     value: number;
     type: string;
   
     constructor(title: string, value: number, type: string) {
-      this.id = Math.random(); // gera um ID numérico aleatório
+      this.id = v4();
       this.title = title;
       this.value = value;
       this.type = type;

be3ee2afef7234b073498afbc0f37a0d71261f99
Fehlpe <felpearcanjo@gmail.com>
2022-12-13 00:33:52 -0300

    delete/put - users

--- apiTransactions/src/index.ts
+++ apiTransactions/src/index.ts
@@ -11,30 +11,49 @@ app.post("/users", (req, res) => {
   const { name, cpf, email, age } = req.body;
 
   if (!name || !cpf || !email || !age) {
-    res.status(400).json({ error: "Dados obrigatórios não registrados" });
+    return res.status(400).json({
+      success: false,
+      message: "Dados obrigatórios não registrados",
+    });
   } else {
     const cpfExists = users.some((user) => user.cpf === cpf);
     if (cpfExists) {
-      return res.status(400).json({ error: "CPF já está em uso" });
+      return res.status(404).json({
+        success: false,
+        message: "CPF já esta em uso!",
+      });
     }
     const user = new User(name, cpf, email, age);
     users.push(user);
-    return res.json(user);
+    return res.status(200).json({
+      success: true,
+      data: user,
+    });
   }
 });
 
 app.get("/users/:id", (req, res) => {
   const id = Number(req.params.id);
   if (!id) {
-    res.status(418).json({ error: "Necessário informar ID" });
+    return res.status(404).json({
+      success: false,
+      message: "Necessário informar ID",
+    });
   } else {
     const user = users.find((user) => user.id === id);
 
     if (!user) {
-      return res.status(404).send("Usuário não encontrado.");
+      return res.status(404).json({
+        success: false,
+        message: "Usuário não encontrado",
+      });
     }
 
     res.send(user);
+    return res.status(200).json({
+      success: true,
+      data: user,
+    });
   }
 });
 
@@ -43,38 +62,120 @@ app.get("/users", (req, res) => {
 
   if (name) {
     const user = users.find((user) => user.name === name);
-    
-    if(user){
-        return res.json({
-            sucesso: true,
-            data:{
-                      id: user.id,
-                      name: user.name,
-                      cpf: user.cpf,
-                      email: user.email,
-                      age: user.age,
-                  }
-                
-        });
+
+    if (user) {
+      return res.status(200).json({
+        sucess: true,
+        data: {
+          id: user.id,
+          name: user.name,
+          cpf: user.cpf,
+          email: user.email,
+          age: user.age,
+        },
+      });
     } else {
-        res.status(400).json({ error: "Nome não registrado no sistema"})
+      return res.status(404).json({
+        success: false,
+        message: "Nome não registrado no sistema",
+      });
     }
   } else if (email) {
-    return res.json({
-      sucesso: true,
-      data: users.filter((user) => user.email === email),
-    });
-  } else if (Number(cpf)) {
-    return res.json({
-      sucesso: true,
-      data: users.filter((user) => user.cpf === cpf),
-    });
+    const user = users.find((user) => user.email === email);
+
+    if (user) {
+      return res.status(200).json({
+        sucess: true,
+        data: {
+          id: user.id,
+          name: user.name,
+          cpf: user.cpf,
+          email: user.email,
+          age: user.age,
+        },
+      });
+    } else {
+      return res.status(404).json({
+        success: false,
+        message: "E-mail não registrado no sistema",
+      });
+    }
+  } else if (cpf) {
+    const user = users.find((user) => user.cpf == cpf);
+
+    if (user) {
+      return res.json({
+        sucess: true,
+        data: {
+          id: user.id,
+          name: user.name,
+          cpf: user.cpf,
+          email: user.email,
+          age: user.age,
+        },
+      });
+    } else {
+      return res.status(404).json({
+        success: false,
+        message: "CPF não registrado no sistema",
+      });
+    }
   } else {
     return res.json({
-      sucesso: true,
-      data: users,
+      sucess: true,
+      data: users.map((user) => {
+        return {
+          id: user.id,
+          name: user.name,
+          cpf: user.cpf,
+          email: user.email,
+          age: user.age,
+        };
+      }),
     });
   }
+});
+
+app.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const indice = users.findIndex((user) => user.id == id);
+
+  if (indice === -1) {
+    return res.status(404).json({
+      sucess: false,
+      message: "Usuário não encontrado",
+    });
+  }
+
+  users.splice(indice, 1);
+
+  res.status(200).json({
+    sucess: true,
+    message: "Usuário removido",
+  });
+});
+
+app.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, cpf, email, age } = req.body;
+
+  const user = users.find((user) => user.id == id);
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "Usuário não encontrado",
+    });
+  }
+
+  name ? (user.name = name) : null;
+  cpf ? (user.cpf = cpf) : null;
+  email ? (user.email = email) : null;
+  age ? (user.age = age) : null;
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
 });
 
 app.listen(8081, () => console.log("Server OK"));
